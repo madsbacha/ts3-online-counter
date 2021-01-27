c1b9511a4a5717aa2f099adfde05a6d38ca0ee6c
Mads <madsbaan@gmail.com>
2021-01-27 14:50:04 +0100

    try catch the redis connection

--- src/api.py
+++ src/api.py
@@ -6,20 +6,31 @@ import base64
 app = Flask(__name__)
 @app.route("/")
 def client_count():
-    r = Redis(env("REDIS_HOST", "localhost"), env("REDIS_PORT", 6379, int), db=0)
-    counter = r.get('counter')
-    counter = counter.decode("utf8") if counter else None
-    usernames = r.get('usernames')
-    usernames = [base64.b64decode(x.encode('utf8')).decode('utf8') for x in usernames.decode("utf8").split(',')] if usernames else []
+    counter = "0"
+    usernames = []
+    try:
+        r = Redis(env("REDIS_HOST", "localhost"), env("REDIS_PORT", 6379, int), db=0)
+        counter = r.get('counter')
+        counter = counter.decode("utf8") if counter else None
+        usernames = r.get('usernames')
+        usernames = [base64.b64decode(x.encode('utf8')).decode('utf8') for x in usernames.decode("utf8").split(',')] if usernames else []
+    except Exception:
+        pass
     return render_template("index.html", online_count=counter, usernames=usernames)
 
 @app.route("/api")
 def client_count_api():
-    r = Redis(env("REDIS_HOST", "localhost"), env("REDIS_PORT", 6379, int), db=0)
-    counter = r.get('counter')
-    counter = counter.decode("utf8") if counter else None
-    usernames = r.get('usernames')
-    usernames = [base64.b64decode(x.encode('utf8')).decode('utf8') for x in usernames.decode("utf8").split(',')] if usernames else []
+    counter = "0"
+    usernames = []
+    try:
+        r = Redis(env("REDIS_HOST", "localhost"), env("REDIS_PORT", 6379, int), db=0)
+        counter = r.get('counter')
+        counter = counter.decode("utf8") if counter else None
+        usernames = r.get('usernames')
+        usernames = [base64.b64decode(x.encode('utf8')).decode('utf8') for x in usernames.decode("utf8").split(',')] if usernames else []
+    except Exception:
+        pass
+
     return {
       'count': counter,
       'users': usernames
